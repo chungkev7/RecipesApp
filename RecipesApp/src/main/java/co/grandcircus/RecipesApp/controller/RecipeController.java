cc7f23f4fb356f1ee3b15b9e899e378ee9409440
Kevin Chung <chungkev7@gmail.com>
2019-12-03 17:53:37 -0500

    added to gitignore

---
+++ RecipesApp/src/main/java/co/grandcircus/RecipesApp/controller/RecipeController.java
@@ -0,0 +1,37 @@
+package co.grandcircus.RecipesApp.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class RecipeController {
+
+	@Value("${recipe.key}")
+	String recipeKey;
+
+	@Value("${application.id}")
+	String appId;
+
+	RestTemplate rt = new RestTemplate();
+
+	@RequestMapping("/")
+	public ModelAndView homePage() {
+
+		return new ModelAndView("index");
+	}
+
+	@RequestMapping("/search")
+	public ModelAndView searchRecipe() {
+
+		String url = "https://api.edamam.com/search?q=chicken&app_id=" + appId + "&app_key=" + recipeKey;
+
+		String test = rt.getForObject(url, String.class);
+
+		System.out.println(test);
+
+		return new ModelAndView("redirect:/");
+	}
+}
